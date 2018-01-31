c2fd164917438203c6b2f474d6bda81791d4eb6f
Phoebe Lau <phoebelau7@gmail.com>
2018-01-31 16:23:17 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,7 +25,9 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(post_params)
-    
+    2.times do
+      @post.tags.build
+    end
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
